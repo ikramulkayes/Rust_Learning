3ef7232d3c358e8feb3be4f5ba492532743a6595
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2024-03-12 17:21:55 +0600

    2

---
+++ src/vars.rs
@@ -0,0 +1,12 @@
+pub fn run() {
+
+    let name = "Brad";
+    let mut age = 38;
+    age = 20;
+
+    println!("{} is my name! I am {}",name,age);
+    //Defining constant
+    const ID:i32 = 001;
+    println!("ID: {}",ID);
+
+}
