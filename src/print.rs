a9e338e2448bb4c62aed28276c4b2be55731425f
MD Ikramul Kayes <ikramul.kayesgg@gmail.com>
2024-03-12 17:04:28 +0600

    gg

---
+++ src/print.rs
@@ -0,0 +1,16 @@
+pub fn run() {
+    println!("Hello, world!");
+    // Basic Formating
+    println!("The Beautiful cat {}'s Number is: {}","Mo",2);
+    // Named Arguments
+
+//tempcomment
+    println!("{name1} is fond of {name2}",name1="Kamal",name2="Kamali");
+
+    //place holder debug trait
+    println!("{:?}",(12,true,"GG"));
+
+    //calculations
+    println!("10+2={}",10+2);
+
+}
